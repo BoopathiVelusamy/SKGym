f882d01f76483c075b81a68002f6088387c0a04a
boopathivelusamy <boopathi0609@gmail.com>
2023-11-21 12:09:52 +0530

    Initial commit

---
+++ app/src/main/java/boopathi/app/skgym/MainActivity.java
@@ -0,0 +1,21 @@
+package boopathi.app.skgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.logo_img);
+
+        Glide.with(this).load(R.drawable.s_screen).into(imageView);
+    }
+}
